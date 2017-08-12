df25d38dfae275a1ed871d460408cdf65d861eb1
Barend Erasmus <Barend Erasmus@DESKTOP-8GS432K>
2017-08-12 18:41:40 +0200

    update

--- src/router.ts
+++ src/router.ts
@@ -15,8 +15,8 @@ export function OAuth2FrameworkRouter(framework: OAuth2Framework, loginPagePath:
      * @apiName AuthorizationRequest
      * @apiGroup OAuth2
      *
-     * @apiParam {String} response_type The authorization endpoint is used by the authorization code grant type and implicit grant type flows
-     * @apiParam {String} client_id The client identifier issued to the client during the registration process
+     * @apiParam {string} response_type The authorization endpoint is used by the authorization code grant type and implicit grant type flows
+     * @apiParam {string} client_id The client identifier issued to the client during the registration process
      * @apiParam {string} redirect_uri After completing its interaction with the resource owner, the authorization server directs the resource owner's user-agent back to the client
      * @apiParam {string} scope The scope of the access request
      * @apiParam {string} state An opaque value used by the client to maintain state between the request and callback
@@ -55,8 +55,8 @@ export function OAuth2FrameworkRouter(framework: OAuth2Framework, loginPagePath:
      * @apiName AccessTokenRequest
      * @apiGroup OAuth2
      *
-     * @apiParam {String} grant_type Grant Type
-     * @apiParam {String} client_id The client identifier issued to the client during the registration process
+     * @apiParam {string} grant_type Grant Type
+     * @apiParam {string} client_id The client identifier issued to the client during the registration process
      * @apiParam {string} client_secret The client secret
      * @apiParam {string} redirect_uri After completing its interaction with the resource owner, the authorization server directs the resource owner's user-agent back to the client
      * @apiParam {string} scope The scope of the access request
